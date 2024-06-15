b9dd62462a3287497e0e52d27056e2325af34128
Jason <haruspexending@gmail.com>
2024-06-15 19:22:37 -0400

    camtriggeraudio

--- Script/Cam1Trigger.gd
+++ Script/Cam1Trigger.gd
@@ -5,9 +5,7 @@ extends Area3D
 @onready var collision_shape_3d_2 = $"../Cam1Trigger/CollisionShape3D2"
 @onready var collision_shape_3d = $CollisionShape3D
 
-
 func _on_body_entered(body):
 	if "Player" in body.name:
 		cam_1.current = true
 		cam_2.current = false
-
