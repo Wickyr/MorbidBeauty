2da0c4c93acaaeeb767b4453655d52af56841097
Jason <haruspexending@gmail.com>
2024-06-16 21:32:54 -0400

    audio commit

--- GeneratedSoundBanks/wwise_ids.gd
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -2,58 +2,58 @@ class_name AK
 
 class EVENTS:
 
-	const MOAN = 3229313568
-	const DEATH = 779278001
-	const RATDEATH = 3924734864
-	const RATSQUEAK = 1690196772
-	const DAMAGE = 1786804762
-	const WALKING = 340271938
-	const BITE = 1645043423
-	const CHASE = 417490929
-	const ACTIVE = 58138747
-	const RATKILL = 986307634
-	const IDLE = 1874288895
-	const PUDDLE = 2003927601
-	const FIRSTSIGHT = 425610712
-	const RATWALKING = 3790712335
-	const ATTACK = 180661997
-	const WALK = 2108779966
-	const AMBIENCE = 85412153
-	const ROCKSCRUMBLING = 2691985942
-	const STOMP = 1166633136
-	const BLOOD = 3934470635
 	const MUSIC = 3991942870
+	const MOAN = 3229313568
+	const MAINMENU = 3604647259
 	const INTRO = 1125500713
 	const NOTE = 849560917
+	const AMBIENCE = 85412153
+	const BLOOD = 3934470635
+	const ROCKSCRUMBLING = 2691985942
 	const CREDITS = 2201105581
-	const MAINMENU = 3604647259
+	const RATWALKING = 3790712335
+	const DAMAGE = 1786804762
+	const STOMP = 1166633136
+	const BITE = 1645043423
+	const RATSQUEAK = 1690196772
+	const IDLE = 1874288895
+	const WALKING = 340271938
+	const PUDDLE = 2003927601
+	const ACTIVE = 58138747
+	const DEATH = 779278001
+	const CHASE = 417490929
+	const RATDEATH = 3924734864
+	const WALK = 2108779966
+	const ATTACK = 180661997
+	const RATKILL = 986307634
+	const FIRSTSIGHT = 425610712
 
 	const _dict = {
-		"Moan": MOAN,
-		"Death": DEATH,
-		"RatDeath": RATDEATH,
-		"RatSqueak": RATSQUEAK,
-		"Damage": DAMAGE,
-		"Walking": WALKING,
-		"Bite": BITE,
-		"Chase": CHASE,
-		"Active": ACTIVE,
-		"RatKill": RATKILL,
-		"Idle": IDLE,
-		"Puddle": PUDDLE,
-		"FirstSight": FIRSTSIGHT,
-		"RatWalking": RATWALKING,
-		"Attack": ATTACK,
-		"Walk": WALK,
-		"Ambience": AMBIENCE,
-		"RocksCrumbling": ROCKSCRUMBLING,
-		"Stomp": STOMP,
-		"Blood": BLOOD,
 		"Music": MUSIC,
+		"Moan": MOAN,
+		"MainMenu": MAINMENU,
 		"Intro": INTRO,
 		"Note": NOTE,
+		"Ambience": AMBIENCE,
+		"Blood": BLOOD,
+		"RocksCrumbling": ROCKSCRUMBLING,
 		"Credits": CREDITS,
-		"MainMenu": MAINMENU
+		"RatWalking": RATWALKING,
+		"Damage": DAMAGE,
+		"Stomp": STOMP,
+		"Bite": BITE,
+		"RatSqueak": RATSQUEAK,
+		"Idle": IDLE,
+		"Walking": WALKING,
+		"Puddle": PUDDLE,
+		"Active": ACTIVE,
+		"Death": DEATH,
+		"Chase": CHASE,
+		"RatDeath": RATDEATH,
+		"Walk": WALK,
+		"Attack": ATTACK,
+		"RatKill": RATKILL,
+		"FirstSight": FIRSTSIGHT
 	}
 
 class STATES:
@@ -66,28 +66,28 @@ class SWITCHES:
 
 class GAME_PARAMETERS:
 
-	const SS_AIR_TURBULENCE = 4160247818
-	const SS_AIR_PRESENCE = 3847924954
 	const SS_AIR_MONTH = 2648548617
-	const SS_AIR_FEAR = 1351367891
-	const SS_AIR_SIZE = 3074696722
-	const SS_AIR_STORM = 3715662592
-	const SS_AIR_TIMEOFDAY = 3203397129
-	const SS_AIR_RPM = 822163944
-	const SS_AIR_FREEFALL = 3002758120
 	const SS_AIR_FURY = 1029930033
+	const SS_AIR_TIMEOFDAY = 3203397129
+	const SS_AIR_TURBULENCE = 4160247818
+	const SS_AIR_FEAR = 1351367891
+	const SS_AIR_STORM = 3715662592
+	const SS_AIR_SIZE = 3074696722
+	const SS_AIR_FREEFALL = 3002758120
+	const SS_AIR_PRESENCE = 3847924954
+	const SS_AIR_RPM = 822163944
 
 	const _dict = {
-		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
-		"SS_Air_Presence": SS_AIR_PRESENCE,
 		"SS_Air_Month": SS_AIR_MONTH,
-		"SS_Air_Fear": SS_AIR_FEAR,
-		"SS_Air_Size": SS_AIR_SIZE,
-		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_Fury": SS_AIR_FURY,
 		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
-		"SS_Air_RPM": SS_AIR_RPM,
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"SS_Air_Fear": SS_AIR_FEAR,
+		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_Size": SS_AIR_SIZE,
 		"SS_Air_Freefall": SS_AIR_FREEFALL,
-		"SS_Air_Fury": SS_AIR_FURY
+		"SS_Air_Presence": SS_AIR_PRESENCE,
+		"SS_Air_RPM": SS_AIR_RPM
 	}
 
 class TRIGGERS:
@@ -98,10 +98,16 @@ class BANKS:
 
 	const INIT = 1355168291
 	const MAIN = 3161908922
+	const RAT = 1047714102
+	const LINWOOD = 3513125265
+	const PLAYER = 1069431850
 
 	const _dict = {
 		"Init": INIT,
-		"Main": MAIN
+		"Main": MAIN,
+		"Rat": RAT,
+		"Linwood": LINWOOD,
+		"Player": PLAYER
 	}
 
 class BUSSES:
