1a419002baa6b059468078edbdb693231cde5152
Jason <haruspexending@gmail.com>
2024-06-16 21:27:30 -0400

    h

--- GeneratedSoundBanks/wwise_ids.gd
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -2,54 +2,58 @@ class_name AK
 
 class EVENTS:
 
-	const AMBIENCE = 85412153
+	const MOAN = 3229313568
+	const DEATH = 779278001
 	const RATDEATH = 3924734864
-	const RATWALKING = 3790712335
+	const RATSQUEAK = 1690196772
+	const DAMAGE = 1786804762
 	const WALKING = 340271938
-	const STOMP = 1166633136
+	const BITE = 1645043423
 	const CHASE = 417490929
 	const ACTIVE = 58138747
-	const WALK = 2108779966
-	const IDLE = 1874288895
-	const MOAN = 3229313568
-	const RATSQUEAK = 1690196772
-	const BITE = 1645043423
-	const DAMAGE = 1786804762
-	const BLOOD = 3934470635
-	const FIRSTSIGHT = 425610712
-	const NOTE = 849560917
-	const ATTACK = 180661997
-	const MUSIC = 3991942870
 	const RATKILL = 986307634
-	const DEATH = 779278001
-	const INTRO = 1125500713
-	const ROCKSCRUMBLING = 2691985942
+	const IDLE = 1874288895
 	const PUDDLE = 2003927601
+	const FIRSTSIGHT = 425610712
+	const RATWALKING = 3790712335
+	const ATTACK = 180661997
+	const WALK = 2108779966
+	const AMBIENCE = 85412153
+	const ROCKSCRUMBLING = 2691985942
+	const STOMP = 1166633136
+	const BLOOD = 3934470635
+	const MUSIC = 3991942870
+	const INTRO = 1125500713
+	const NOTE = 849560917
+	const CREDITS = 2201105581
+	const MAINMENU = 3604647259
 
 	const _dict = {
-		"Ambience": AMBIENCE,
+		"Moan": MOAN,
+		"Death": DEATH,
 		"RatDeath": RATDEATH,
-		"RatWalking": RATWALKING,
+		"RatSqueak": RATSQUEAK,
+		"Damage": DAMAGE,
 		"Walking": WALKING,
-		"Stomp": STOMP,
+		"Bite": BITE,
 		"Chase": CHASE,
 		"Active": ACTIVE,
-		"Walk": WALK,
-		"Idle": IDLE,
-		"Moan": MOAN,
-		"RatSqueak": RATSQUEAK,
-		"Bite": BITE,
-		"Damage": DAMAGE,
-		"Blood": BLOOD,
-		"FirstSight": FIRSTSIGHT,
-		"Note": NOTE,
-		"Attack": ATTACK,
-		"Music": MUSIC,
 		"RatKill": RATKILL,
-		"Death": DEATH,
-		"Intro": INTRO,
+		"Idle": IDLE,
+		"Puddle": PUDDLE,
+		"FirstSight": FIRSTSIGHT,
+		"RatWalking": RATWALKING,
+		"Attack": ATTACK,
+		"Walk": WALK,
+		"Ambience": AMBIENCE,
 		"RocksCrumbling": ROCKSCRUMBLING,
-		"Puddle": PUDDLE
+		"Stomp": STOMP,
+		"Blood": BLOOD,
+		"Music": MUSIC,
+		"Intro": INTRO,
+		"Note": NOTE,
+		"Credits": CREDITS,
+		"MainMenu": MAINMENU
 	}
 
 class STATES:
@@ -62,28 +66,28 @@ class SWITCHES:
 
 class GAME_PARAMETERS:
 
+	const SS_AIR_TURBULENCE = 4160247818
+	const SS_AIR_PRESENCE = 3847924954
+	const SS_AIR_MONTH = 2648548617
 	const SS_AIR_FEAR = 1351367891
 	const SS_AIR_SIZE = 3074696722
-	const SS_AIR_TURBULENCE = 4160247818
+	const SS_AIR_STORM = 3715662592
+	const SS_AIR_TIMEOFDAY = 3203397129
+	const SS_AIR_RPM = 822163944
 	const SS_AIR_FREEFALL = 3002758120
 	const SS_AIR_FURY = 1029930033
-	const SS_AIR_PRESENCE = 3847924954
-	const SS_AIR_STORM = 3715662592
-	const SS_AIR_RPM = 822163944
-	const SS_AIR_MONTH = 2648548617
-	const SS_AIR_TIMEOFDAY = 3203397129
 
 	const _dict = {
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"SS_Air_Presence": SS_AIR_PRESENCE,
+		"SS_Air_Month": SS_AIR_MONTH,
 		"SS_Air_Fear": SS_AIR_FEAR,
 		"SS_Air_Size": SS_AIR_SIZE,
-		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
-		"SS_Air_Freefall": SS_AIR_FREEFALL,
-		"SS_Air_Fury": SS_AIR_FURY,
-		"SS_Air_Presence": SS_AIR_PRESENCE,
 		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
 		"SS_Air_RPM": SS_AIR_RPM,
-		"SS_Air_Month": SS_AIR_MONTH,
-		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY
+		"SS_Air_Freefall": SS_AIR_FREEFALL,
+		"SS_Air_Fury": SS_AIR_FURY
 	}
 
 class TRIGGERS:
@@ -114,12 +118,12 @@ class AUX_BUSSES:
 
 class AUDIO_DEVICES:
 
-	const NO_OUTPUT = 2317455096
 	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
 
 	const _dict = {
-		"No_Output": NO_OUTPUT,
-		"System": SYSTEM
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
 	}
 
 class EXTERNAL_SOURCES:
---
+++ Scenes/Smash.gd
@@ -0,0 +1,5 @@
+extends Area3D
+
+func _on_body_entered(body):
+	if "Player" in body.name:
+
