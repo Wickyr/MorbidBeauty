b3c56136e014e06fb99e76708d35b05cae4a2a60
Jason <haruspexending@gmail.com>
2024-06-16 20:29:55 -0400

    push

--- Script/Pit.gd
+++ Script/Pit.gd
@@ -5,3 +5,6 @@ extends Area3D
 func _on_body_entered(body):
 	if "Player" in body.name:
 		player.health = 0
+		Wwise.register_game_obj(self, self.name)
+		Wwise.set_3d_position(self, transform)
+		Wwise.post_event_id(AK.EVENTS.DEATH, self)
--- Script/puddle.gd
+++ Script/puddle.gd
@@ -10,6 +10,9 @@ func _ready():
 func _on_area_3d_body_entered(body):
 	if "Player" in body.name:
 		player.noisy = true
+		Wwise.register_game_obj(self, self.name)
+		Wwise.set_3d_position(self, transform)
+		Wwise.post_event_id(AK.EVENTS.PUDDLE, self)
 		print("Noisy")
 
 
